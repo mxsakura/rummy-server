8686427eb131c8a5145c8014dbe6b19298c7cff8
mxsakura <1440717174@qq.com>
2021-03-17 11:36:32 +0800

    提交文件

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
+
+var app = express();
+
+// //设置允许跨域请求
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); //访问控制允许来源：所有
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); //访问控制允许报头 X-Requested-With: xhr请求
+  res.header('Access-Control-Allow-Metheds', 'PUT, POST, GET, DELETE, OPTIONS'); //访问控制允许方法
+  res.header('X-Powered-By', 'nodejs'); //自定义头信息，表示服务端用nodejs
+  // res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+//实现静态资源访问功能
+// app.use('/public',express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,113 @@
+const e = require('express');
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path'); //系统路径模块
+
+/* GET api. */
+router.get('/', function (req, res, next) {
+    res.send("请求正常");
+});
+
+router.get('/banner', function (req, res, next) {
+    var file = path.join(__dirname, '../public/data/banner.json'); //文件路径，__dirname为当前运行js文件的目录
+    console.log(file)
+    //读取json文件
+    fs.readFile(file, 'utf-8', function (err, data) {
+        if (err) {
+            res.send('请求失败');
+        } else {
+            res.send(data);
+        }
+    });
+});
+router.get('/recommended', function (req, res, next) {
+    var file = path.join(__dirname, '../public/data/recommended.json'); //文件路径，__dirname为当前运行js文件的目录
+    console.log(file)
+    //读取json文件
+    fs.readFile(file, 'utf-8', function (err, data) {
+        if (err) {
+            res.send({
+                code: 500,
+                msg: '请求失败'
+            });
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+router.get('/applist', function (req, res, next) {
+    var file = path.join(__dirname, '../public/data/applist.json'); //文件路径，__dirname为当前运行js文件的目录
+    console.log(file)
+    //读取json文件
+    fs.readFile(file, 'utf-8', function (err, data) {
+        if (err) {
+            res.send('请求失败');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+router.get('/recommended', function (req, res, next) {
+    var file = path.join(__dirname, '../public/data/recommended.json'); //文件路径，__dirname为当前运行js文件的目录
+    console.log(file)
+    //读取json文件
+    fs.readFile(file, 'utf-8', function (err, data) {
+        if (err) {
+            res.send('请求失败');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+// TODO 查询配置文件或者数据库信息来确认是否有更新  
+function checkUpdate(params, callback) {
+    let result = {
+        appVersion: "1.0.1",
+        wgtUrl: "http://172.16.20.114:3000/file/__UNI__8F97DDA.wgt",
+        pkgUrl: "http://172.16.20.114:3000/file/"
+    }
+    // 这里简单判定下，不相等就是有更新。
+    var currentVersions = params.appVersion.split('.');
+    var resultVersions = result.appVersion.split('.');
+
+    if (currentVersions[0] < resultVersions[0]) {
+        // 说明有大版本更新  
+        callback({
+            update: true,
+            wgtUrl: '',
+            pkgUrl: result.pkgUrl
+        })
+    } else if (params.appVersion < result.appVersion) {
+        // 其它情况均认为是小版本更新  
+        callback({
+            update: true,
+            wgtUrl: result.wgtUrl,
+            pkgUrl: ''
+        })
+    } else {
+        // 无需更新
+        callback({
+            update: false,
+            wgtUrl: '',
+            pkgUrl: ''
+        })
+    }
+}
+
+
+router.get('/app_update', function (req, res) {
+    var appName = req.query.name;
+    var appVersion = req.query.version;
+    checkUpdate({
+        appName: appName,
+        appVersion: appVersion
+    }, function (result) {
+        res.send(result);
+    });
+})
+
+module.exports = router;
